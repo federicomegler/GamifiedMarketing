c041bfe6ac693c64b2fc3c9c1fd1274da2de27f5
MorganaGiorgio <MorganaGiorgio@LAPTOP-28I9CBP4.lan>
2021-03-23 16:37:34 +0100

    aggiungo answer

---
+++ GamifiedMarketingEJB/ejbModule/entities/Answer.java
@@ -0,0 +1,41 @@
+
+package entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Answer implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String content;
+	
+	@ManyToOne
+	@JoinColumn(name = "user")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name = "question")
+	private Question question;
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
+
+
+//megler cazzone
