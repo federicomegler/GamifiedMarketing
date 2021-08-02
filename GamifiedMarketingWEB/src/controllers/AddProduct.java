5c5bdc49df13399be4bd2dab88302835b587bfe1
rossigiorgio <rossi_giorgio@outlook.it>
2021-08-02 14:41:09 +0200

    Pagina Html per creazione questionario.
    AddProduct fa redirect sulla pagina html di creazione

--- GamifiedMarketingWEB/src/controllers/AddProduct.java
+++ GamifiedMarketingWEB/src/controllers/AddProduct.java
@@ -1,19 +1,30 @@
 package controllers;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import entities.User;
 
 /**
  * Servlet implementation class AddQuestionnaire
  */
-@WebServlet("/AddQuestionnaire")
+@WebServlet("/AddProduct")
 public class AddProduct extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private TemplateEngine templateEngine;  
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -21,13 +32,32 @@ public class AddProduct extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String path = "/WEB-INF/ProductCreation.html";
+		ServletContext servletContext = getServletContext();
+		HttpSession session = request.getSession();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		if(session.isNew() || session.getAttribute("user") == null || ((User) session.getAttribute("user")).getAdmin()==0) {
+			path = getServletContext().getContextPath() + "/index.html";
+			response.sendRedirect(path);
+		}
+		else {
+			//ctx.setVariable("user", ((User)session.getAttribute("user")));
+			RequestDispatcher rd=request.getRequestDispatcher(path);
+			rd.forward(request, response);
+		}
 	}
 
 	/**
