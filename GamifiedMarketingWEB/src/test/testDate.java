ee59840804ccd6fd30ba524008e4af789378012c
rossigiorgio <rossi_giorgio@outlook.it>
2021-08-03 16:28:29 +0200

    pagina di creazione del prodotto funzionante con controlli javascript
    Aggiornato pagina productservice per la data
    
    aggiunto servlet per test

---
+++ GamifiedMarketingWEB/src/test/testDate.java
@@ -0,0 +1,60 @@
+package test;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ * Servlet implementation class testDate
+ */
+@WebServlet("/testDate")
+@MultipartConfig
+public class testDate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public testDate() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//Part image = request.getPart("prodImage");
+		java.util.Date questionnaireDate= new java.util.Date();
+		Calendar cal= Calendar.getInstance();
+		try {
+			questionnaireDate = new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("date"));
+			cal.setTime(questionnaireDate);
+			cal.set(Calendar.HOUR_OF_DAY, 12);
+			questionnaireDate= cal.getTime();
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		};
+		response.getWriter().append("Served at: ").append(questionnaireDate.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
